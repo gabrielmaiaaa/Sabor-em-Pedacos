1a04a5fd453c1c7f9b0e16d307a9d0f4b251092c
Maia <gabrielmaia6743@gmail.com>
2024-10-18 15:20:27 -0300

    inicio do backend

---
+++ Backend/models/Cliente.js
@@ -0,0 +1,11 @@
+class Cliente {
+    constructor(id, username, email, password, endereco) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.endereco = endereco;
+    }
+}
+
+module.exports = Cliente
