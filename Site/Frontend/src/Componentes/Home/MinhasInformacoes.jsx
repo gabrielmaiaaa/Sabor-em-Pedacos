374d8ee72769749d4147682d80d444ded79f2dd1
Maia <gabrielmaia6743@gmail.com>
2024-10-31 22:20:00 -0300

    Arrumei o backend e criei as rotas do frontend, mas ainda não foram implementados para serem acessados

---
+++ Site/Frontend/src/Componentes/Home/MinhasInformacoes.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MinhasInformacoes() {
+  return (
+    <div>MinhasInformacoes</div>
+  )
+}
---
+++ Site/Frontend/src/Componentes/Home/RealizarRelatorio.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RealizarRelatorio() {
+  return (
+    <div>RealizarRelatorio</div>
+  )
+}
