8a165859610575b3162a5aba896e894aa99c21fa
Maia <gabrielmaia6743@gmail.com>
2024-11-04 18:16:48 -0300

    Melhorias no frontend. Agora já posso dar inicio ao processo de comunicação de rotas

--- Site/Frontend/src/Componentes/Home/PaginaInicial.jsx
+++ Site/Frontend/src/Componentes/Home/PaginaInicial.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import '../CSS/Home/PaginaInicial.css';
 
 export default function PaginaInicial() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <div className="App">
       {/* Barra de navegação */}
       <nav className="navbar">
-        <button className="menu-button">☰</button>
-        <a href="#products">Produtos</a>
-        <a href="#menu">Cardápio</a>
-        <a href="#cart">Minhas Compras</a>
-        <input type="text" placeholder="Buscar" className="search-bar" />
+        <button className="menu-button" onClick={toggleMenu}>
+          {isMenuOpen ? '✖' : '☰'} {/* Alterna entre "☰" e "✖" */}
+        </button>
+        <a href="#products" className={`navbar-link ${isMenuOpen ? 'hidden' : ''}`}>Produtos</a>
+        <a href="#menu" className={`navbar-link ${isMenuOpen ? 'hidden' : ''}`}>Cardápio</a>
+        <a href="#cart" className={`navbar-link ${isMenuOpen ? 'hidden' : ''}`}>Minhas Compras</a>
+        <input type="text" placeholder="Buscar" className={`search-bar`} />
       </nav>
+
+      {/* Menu lateral */}
+      {isMenuOpen && (
+        <div className="side-menu">
+          <ul>
+            <li><a href="#products">Produtos</a></li>
+            <li><a href="#menu">Cardápio</a></li>
+            <li><a href="#cart">Minhas Compras</a></li>
+            <li><a href="#order">Meu pedido</a></li>
+            <li><a href="#info">Minhas Informações</a></li>
+            <li><a href="#report">Gerar Relatório</a></li>
+          </ul>
+          <div className="contact-info">
+            <p>Entre em contato</p>
+            <p>Ligue para 38999106743</p>
+            <p>ou mande email para</p>
+            <p>gabrielmaia6743@gmail.com</p>
+          </div>
+        </div>
+      )}
+
+      {/* Overlay cinza quando o menu está aberto */}
+      {isMenuOpen && <div className="overlay" onClick={toggleMenu}></div>}
 
       {/* Imagem do banner */}
       <div className="banner">
